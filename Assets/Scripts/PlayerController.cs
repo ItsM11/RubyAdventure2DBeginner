88010961a1bf4d3dbddf9f1f9aedf30fa3a99ef0
ItsM11 <a59656@alunos.ipb.pt>
2025-03-09 13:15:42 +0000

    Player Character and Movement
    Acabei esse tutorial.
    
    Criei scripts de movimento.
    Fiz com que o movimento se move-se de forma independente aos Frames.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,8 +13,35 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
-        transform.position = position;
+    float horizontal = 0.0f;
+    if (Input.GetKey(KeyCode.LeftArrow))
+    {
+        horizontal = -1.0f;
+ 	    }
+    else if (Input.GetKey(KeyCode.RightArrow))
+    {
+        horizontal = 1.0f;
     }
+    Debug.Log(horizontal);
+
+
+    float vertical = 0.0f;
+    if (Input.GetKey(KeyCode.UpArrow))
+    {
+        vertical = 1.0f;
+    }
+    else if (Input.GetKey(KeyCode.DownArrow))
+    {
+        vertical = -1.0f;
+    }
+    Debug.Log(vertical);
+
+
+    Vector2 position = transform.position;
+    position.x = position.x + 0.1f * horizontal;
+    position.y = position.y + 0.1f * vertical;
+    transform.position = position;
 }
+
+    }
+
